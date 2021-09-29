64f91c79baa3fa81dcd5c78b3ca2a18a1b6804e6
luis <luis.w0587@gmail.com>
2021-09-29 13:49:28 -0500

    añadido funcion de medicion de tiempo

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,17 +1,29 @@
 import "./feed.css";
 import Share from '../../components/share/Share';
 import Post from '../../components/post/Post';
-import {Posts} from '../../dummyData';
+//import {Posts} from '../../dummyData';
+import {useState,useEffect} from 'react';
+import axios from 'axios';
 
-export default function feed() {
+export default function Feed() {
+    const [posts,setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async()=>{
+            const res = await axios.get("/posts/timeline/6152a017b81c71d4b856dfa3");
+           setPosts(res.data);
+        }
+        fetchPosts();
+    },[])
+
     return (
         <div className="feed">
             <div className="feedWrapper">
                 <Share/>
-                {Posts.map(p=>(
-                     <Post key={p.id} post={p}/>
+                {posts.map(p=>(
+                     <Post key={p._id} post={p}/>
                 ))}
-               
+            
             </div>
         </div>
     )
